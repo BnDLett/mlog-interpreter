8f07aeb8e87a7d83881fcf781a4574b0c81ffc0c
BnDLett <lett@mail.lettsn.org>
2025-10-20 12:25:35 -0400

    Add validation.c
    My laptop is about dead so I'll work on this at home

---
+++ src/validation.c
@@ -0,0 +1,10 @@
+//
+// Created by lett on 10/20/25.
+//
+
+#include "error.h"
+
+// Validates the callbacks in a program of tokens.
+struct Error *validate_many(struct Line *program, struct GlobalState *global_state) {
+
+}
